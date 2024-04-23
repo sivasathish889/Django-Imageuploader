63e7ba6216ccd6e9bb032e634a46d03b7e5b283f
sivasathish889 <sivasathish889@gmail.com>
2024-04-23 14:26:12 +0530

    come changes

--- ImageProject/urls.py
+++ ImageProject/urls.py
@@ -6,7 +6,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",views.imageview),
-    path("delete/<id>/",views.Delete),
+    path("",views.imageview, name="home"),
+    path("delete/<id>/",views.Delete, name="delete"),
     path("delete/",views.deleted)
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_DIR)
